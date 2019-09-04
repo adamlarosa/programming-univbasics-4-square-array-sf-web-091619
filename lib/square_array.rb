c56bd954d7793e2d589f37596605f7235438edbd
Adam La Rosa <adam.larosa@gmail.com>
2019-09-04 00:46:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,12 +2,9 @@ def square_array(array)
   count = 0
   
   while count < array.length
-    
     array[count] = array[count] * array[count]
-    
     count += 1
-    
-    
   end
+  
   return array
 end
